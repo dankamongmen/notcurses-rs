d132c12ded1d8c58fd4981fa05620feb5f4f328b
joseLuís <joseluis@andamira.net>
2021-06-18 10:10:21 +0200

    add Style::UNDERCURL

--- src/style.rs
+++ src/style.rs
@@ -5,23 +5,35 @@ use crate::sys;
 bitflags! {
     /// The style to apply to the text.
     pub struct Style: u16 {
+        // The bitmask covering all the styles.
+        // const MASK = sys::ffi::NCSTYLE_MASK as u16;
+
         /// Blinking.
-        const BLINK= sys::ffi::NCSTYLE_BLINK as u16;
+        const UNDERCURL= sys::ffi::NCSTYLE_UNDERCURL as u16;
 
-        /// Extra bright or bold.
-        const BOLD = sys::ffi::NCSTYLE_BOLD as u16;
-
-        /// Half bright.
-        const DIM = sys::ffi::NCSTYLE_DIM as u16;
-
-        /// Invisible or blank mode.
-        const INVIS = sys::ffi::NCSTYLE_INVIS as u16;
+        /// Strikethrough.
+        const STRUCK = sys::ffi::NCSTYLE_STRUCK as u16;
 
         /// Italic.
         const ITALIC = sys::ffi::NCSTYLE_ITALIC as u16;
 
-        /// No style.
-        const NONE = sys::ffi::NCSTYLE_NONE as u16;
+        /// Best highlighting mode of the terminal.
+        const STANDOUT = sys::ffi::NCSTYLE_STANDOUT as u16;
+
+        /// Underlining.
+        const UNDERLINE = sys::ffi::NCSTYLE_UNDERLINE as u16;
+
+        /// Blinking.
+        const BLINK= sys::ffi::NCSTYLE_BLINK as u16;
+
+        /// Half bright.
+        const DIM = sys::ffi::NCSTYLE_DIM as u16;
+
+        /// Extra bright or bold.
+        const BOLD = sys::ffi::NCSTYLE_BOLD as u16;
+
+        /// Invisible or blank mode.
+        const INVIS = sys::ffi::NCSTYLE_INVIS as u16;
 
         /// Protected mode.
         const PROTECT = sys::ffi::NCSTYLE_PROTECT as u16;
@@ -29,16 +41,7 @@ bitflags! {
         /// Reverse video.
         const REVERSE = sys::ffi::NCSTYLE_REVERSE as u16;
 
-        /// Best highlighting mode of the terminal.
-        const STANDOUT = sys::ffi::NCSTYLE_STANDOUT as u16;
-
-        /// Strikethrough.
-        const STRUCK = sys::ffi::NCSTYLE_STRUCK as u16;
-
-        /// Underlining.
-        const UNDERLINE = sys::ffi::NCSTYLE_UNDERLINE as u16;
-
-        // The bitmask covering all the styles.
-        // const MASK = sys::ffi::NCSTYLE_MASK as u16;
+        /// No style.
+        const NONE = sys::ffi::NCSTYLE_NONE as u16;
     }
 }
